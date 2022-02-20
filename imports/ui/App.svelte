b8032ef19f6035ccc3d320d935200f9940c592db
Geovanna Carolline <geovanna.carolline.gcs@gmail.com>
2022-02-20 17:55:14 -0400

    first commit

---
+++ imports/ui/App.svelte
@@ -0,0 +1,37 @@
+<script>
+  import { Route, router } from 'tinro';
+  import Login from './components/pages/Login.svelte';
+
+  let counter = 0;
+ 
+  if (!Meteor.userId()) {
+    router.goto('/login');
+  };
+
+  const addToCounter = () => {
+    counter += 1;
+  }
+</script>
+
+{Meteor.userId()}
+<button on:click={Meteor.logout}>Logout</button>
+
+<Route path='/login'>
+  <Login/>
+
+<div class="container">
+  <h1>Welcome to Meteor!</h1>
+  
+  <button on:click={addToCounter}>Click Me</button>
+  <p>You've pressed the button {counter} times.</p>
+
+  <h2>Learn Meteor!</h2>
+  <ul>
+    <li><a href="https://svelte-tutorial.meteor.com/" target="_blank">Do the Tutorial</a></li>
+    <li><a href="http://guide.meteor.com" target="_blank">Follow the Guide</a></li>
+    <li><a href="https://docs.meteor.com" target="_blank">Read the Docs</a></li>
+    <li><a href="https://forums.meteor.com" target="_blank">Discussions</a></li>
+  </ul>
+</div>
+
+</Route>
---
+++ imports/ui/components/pages/Login.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { Accounts } from 'meteor/accounts-base';
+  import { router } from 'tinro';
+
+  let email = '';
+  let password = '';
+
+  // TODO: Fazer componente de registro e arrumar login;
+
+  const handleLogin = () => {
+    Meteor.loginWithPassword({username:email}, password, (error) => {
+      if (error){
+        console.error(error);
+        Accounts.createUser({username:email, password}, console.error);
+      };
+    });
+  }
+</script>
+
+<input bind:value={email} type='email' placeholder='Seu melhor email :D'/>
+<input bind:value={password} type='password'/>
+<button on:click={handleLogin}>Entrar</button>
