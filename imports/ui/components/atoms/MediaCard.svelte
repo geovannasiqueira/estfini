7aba88221494cde23f9b71f00bbc85b86c43e651
Luiz de Oliveira <luiz@noredink.com>
2022-04-03 21:32:31 -0300

    Added icon library and some adjustments to the MediaCard component

--- imports/ui/components/atoms/MediaCard.svelte
+++ imports/ui/components/atoms/MediaCard.svelte
@@ -1,15 +1,23 @@
 <script>
+  import Icon from 'svelte-icons-pack/Icon.svelte';
+  import heartIcon from 'svelte-icons-pack/fa/FaHeart';
+  import solidHeartIcon from 'svelte-icons-pack/fa/FaSolidHeart';
+
   export let media = {};
 </script>
 
 <div class="card shadow bg-base-200">
-  <div class="card-body flex space-x-4">
-    <div class="card-title">
-
-    </div>
+  <div class="card-body flex flex-row items-center space-x-4">
     <div>
-      {media.title}
-      {media.author}
+      <Icon className="fill-current" size="2em" src={solidHeartIcon}/>
+    </div>
+    <div class="flex flex-col">
+      <p>
+        {media.title}
+      </p>
+      <p>
+        {media.author}
+      </p>
     </div>
   </div>
 </div>
