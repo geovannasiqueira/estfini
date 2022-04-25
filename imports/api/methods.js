905f331b3bdc0c300c0313ad7186d8be11c6a2c5
Geovanna Carolline <geovanna.carolline.gcs@gmail.com>
2022-04-24 20:48:05 -0400

    Add Method to search The Movie DB API

--- imports/api/methods.js
+++ imports/api/methods.js
@@ -1,4 +1,5 @@
 import { Meteor } from "meteor/meteor";
+import fetch from "node-fetch";
 import { MediasCollection } from "./MediasCollection";
 import { UsersRecommendationsCollection } from "./UsersRecommendationsCollection";
 
@@ -30,5 +31,24 @@ Meteor.methods({
       userId: this.userId,
       mediaId,
     });
+  },
+
+  "searchTheMovieDb": async function (searchString) {
+    if (!this.userId) {
+      throw new Meteor.Error("Not authorized.");
+    }
+    const { results } = await fetch(`https://api.themoviedb.org/3/search/tv?api_key=${Meteor.settings.theMovieDbApiKey}&language=en-US&page=1&query=${searchString}`)
+      .then(res => {
+        return res.json();
+      } );
+    // TODO: get genres, complete image url
+    return results.map(({poster_path, id, name, overview, original_language, first_air_date}) => ({
+      id,
+      name,
+      overview,
+      originalLanguage: original_language,
+      releaseDate: first_air_date,
+      image: poster_path,
+    }))
   },
 });
