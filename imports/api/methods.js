008e347570beb1706a6334732a287bce28320cec
Vinicius de Castro <viniccastro1@gmail.com>
2022-05-09 22:19:57 -0300

    Did some promise witchery

--- imports/api/methods.js
+++ imports/api/methods.js
@@ -4,7 +4,7 @@ import { MediasCollection } from "./MediasCollection";
 import { UsersRecommendationsCollection } from "./UsersRecommendationsCollection";
 
 Meteor.methods({
-  "media.save": function (media) {
+  "media.recommend": function (theMovieDbId) {
     if (!this.userId) {
       throw new Meteor.Error("Not authorized.");
     }
@@ -22,33 +22,40 @@ Meteor.methods({
     });
   },
 
-  "media.recommend": function (mediaId) {
-    if (!this.userId) {
-      throw new Meteor.Error("Not authorized.");
-    }
-
-    return UsersRecommendationsCollection.insert({
-      userId: this.userId,
-      mediaId,
-    });
-  },
-
   "searchTheMovieDb": async function (searchString) {
     if (!this.userId) {
       throw new Meteor.Error("Not authorized.");
     }
+    console.log(Meteor.settings)
     const { results } = await fetch(`https://api.themoviedb.org/3/search/tv?api_key=${Meteor.settings.theMovieDbApiKey}&language=en-US&page=1&query=${searchString}`)
       .then(res => {
         return res.json();
-      } );
+      });
+    console.log(results)
     // TODO: get genres, complete image url
-    return results.map(({poster_path, id, name, overview, original_language, first_air_date}) => ({
-      id,
-      name,
-      overview,
-      originalLanguage: original_language,
-      releaseDate: first_air_date,
-      image: poster_path,
-    }))
+    return await Promise.all(results.map(
+      async ({
+        poster_path,
+        id,
+        name,
+        overview,
+        original_language,
+        first_air_date
+      }) => {
+        const {status} = await fetch(`https://api.themoviedb.org/3/tv/${id}?api_key=${Meteor.settings.theMovieDbApiKey}`)
+          .then(res => {
+            return res.json();
+          });
+
+        return {
+          id,
+          status,
+          name,
+          overview,
+          originalLanguage: original_language,
+          releaseDate: first_air_date,
+          image: poster_path,
+        }
+      }))
   },
 });
