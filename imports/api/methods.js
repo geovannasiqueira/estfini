0eb0b00605c8ea4f4480e93c6d1c5aa86930bf9d
Geovanna Carolline <geovannacarolline@pop-os.localdomain>
2022-05-17 21:28:06 -0400

    media filter implementation

--- imports/api/methods.js
+++ imports/api/methods.js
@@ -3,12 +3,14 @@ import fetch from "node-fetch";
 import { MediasCollection } from "./MediasCollection";
 import { UsersRecommendationsCollection } from "./UsersRecommendationsCollection";
 
+const MEDIA_STATUS = { ENDED: "Ended" };
+const MIN_MEDIA_AMOUNT = 10;
+
 Meteor.methods({
   "media.recommend": function (theMovieDbId) {
     if (!this.userId) {
       throw new Meteor.Error("Not authorized.");
     }
-    //console.log(media);
     const mediaId = MediasCollection.insert(
       { ...media, createdAt: new Date() },
       (error) => {
@@ -22,40 +24,54 @@ Meteor.methods({
     });
   },
 
-  "searchTheMovieDb": async function (searchString) {
+  searchTheMovieDb: async function (searchString) {
     if (!this.userId) {
       throw new Meteor.Error("Not authorized.");
     }
-    console.log(Meteor.settings)
-    const { results } = await fetch(`https://api.themoviedb.org/3/search/tv?api_key=${Meteor.settings.theMovieDbApiKey}&language=en-US&page=1&query=${searchString}`)
-      .then(res => {
-        return res.json();
-      });
-    console.log(results)
-    // TODO: get genres, complete image url
-    return await Promise.all(results.map(
-      async ({
-        poster_path,
-        id,
-        name,
-        overview,
-        original_language,
-        first_air_date
-      }) => {
-        const {status} = await fetch(`https://api.themoviedb.org/3/tv/${id}?api_key=${Meteor.settings.theMovieDbApiKey}`)
-          .then(res => {
-            return res.json();
-          });
 
-        return {
-          id,
-          status,
-          name,
-          overview,
-          originalLanguage: original_language,
-          releaseDate: first_air_date,
-          image: poster_path,
-        }
-      }))
+    let mediasThatEnded = [];
+    let page = 1;
+    let totalPages = 1;
+    while (mediasThatEnded.length <= MIN_MEDIA_AMOUNT && page <= totalPages) {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/search/tv?api_key=${Meteor.settings.theMovieDbApiKey}&language=en-US&page=${page}&query=${searchString}`
+      );
+      const { results, total_pages } = await response.json();
+      totalPages = total_pages;
+      // TODO: get genres, complete image url
+      const mediasWithStatus = await Promise.all(
+        results.map(
+          async ({
+            poster_path,
+            id,
+            name,
+            overview,
+            original_language,
+            first_air_date,
+          }) => {
+            const { status } = await fetch(
+              `https://api.themoviedb.org/3/tv/${id}?api_key=${Meteor.settings.theMovieDbApiKey}`
+            ).then((res) => {
+              return res.json();
+            });
+
+            return {
+              id,
+              status,
+              name,
+              overview,
+              originalLanguage: original_language,
+              releaseDate: first_air_date,
+              image: poster_path,
+            };
+          }
+        )
+      );
+      mediasThatEnded = [...mediasThatEnded, ...mediasWithStatus.filter(
+        ({ status }) => status === MEDIA_STATUS.ENDED
+      )];
+      page += 1;
+    }
+    return mediasThatEnded;
   },
 });
